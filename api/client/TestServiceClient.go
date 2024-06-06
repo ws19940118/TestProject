31d6541fd3fdf45acd10c74ec5921afb3a00bc40
wangsheng <42263344+ws19940118@users.noreply.github.com>
2024-06-06 11:47:15 +0800

    Update TestServiceClient.go

--- api/client/TestServiceClient.go
+++ api/client/TestServiceClient.go
@@ -10,6 +10,21 @@ import (
 
 // new一个客户端，业务上可以直接调用方法，testservice.Client.GetInfo()
 func NewTestServiceClient() testservice.Client {
+	/*r, err := etcd.NewEtcdResolver([]string{"192.168.1.12:100,192.168.1.13:101"})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fp := retry.NewFailurePolicy()
+	fp.WithMaxRetryTimes(3)                          // 配置最多重试3次
+	cli, err := testservice.NewClient("TestService", // 服务名
+		client.WithRPCTimeout(time.Second*3), // 设置超时时间
+		client.WithFailureRetry(fp),          // 设置失败重试
+		client.WithResolver(r),               // 设置注册中心
+		client.WithMuxConnection(1))          // 设置连接数
+	if err != nil {
+		fmt.Println(err)
+	}
+	return cli*/
 	fp := retry.NewFailurePolicy()
 	fp.WithMaxRetryTimes(3)                          // 配置最多重试3次
 	cli, err := testservice.NewClient("TestService", // 服务名
